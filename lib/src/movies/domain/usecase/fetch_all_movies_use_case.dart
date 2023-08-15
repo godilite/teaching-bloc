b7c01d8cfe92f26c2439f853456c5d28d6295117
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2023-08-15 06:43:32 +0100

    added try and catch to RetryPressed event

--- lib/src/movies/domain/usecase/fetch_all_movies_use_case.dart
+++ lib/src/movies/domain/usecase/fetch_all_movies_use_case.dart
@@ -7,6 +7,8 @@ class FetchMoviesUseCase {
   FetchMoviesUseCase(this.movieRepository);
 
   Future<List<MovieModel>> execute() async {
-    return await movieRepository.fetchAllMovies();
+     final movies = await movieRepository.fetchAllMovies();
+    movies.sort();
+    return movies;
   }
 }

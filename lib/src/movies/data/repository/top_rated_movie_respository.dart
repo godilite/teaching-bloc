ac93a6cf4c77714a145164db58353f5ec91bffea
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2023-09-20 05:15:39 +0100

    Added API for some movies

---
+++ lib/src/movies/data/repository/top_rated_movie_respository.dart
@@ -0,0 +1,24 @@
+import 'package:teaching_bloc/src/movies/domain/models/movie_model.dart';
+import 'package:teaching_bloc/src/movies/domain/repository/movie_repository.dart';
+import 'package:teaching_bloc/src/network/base_api_client.dart';
+
+class TopRatedMovieRepositoryImpl implements MovieRepository {
+  final String apiKey = '9615d585fe799a30b4302e086d95622e';
+  static String movieURL = 'https://api.themoviedb.org/3/movie/top_rated';
+  final ApiClient _client;
+  TopRatedMovieRepositoryImpl(this._client);
+  @override
+  Future<List<MovieModel>> fetchAllMovies() async {
+    final params = {
+      'api_key': apiKey,
+      'language': 'en-US',
+    };
+
+    final response = await _client.get(movieURL, queryParameters: params);
+
+    final movieList = response['results'] as List<dynamic>;
+    return movieList
+        .map((movie) => MovieModel.fromJson(movie as Map<String, dynamic>))
+        .toList();
+  }
+}
